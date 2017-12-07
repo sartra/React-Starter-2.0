54bd6b852fc8a3b4fca9131e62984968fa29a4bb
Daniel Zuzevich <dzuzevich@graftonstudio.com>
2017-12-06 19:15:43 -0500

    Removed react router redux because it sucks

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux'
-import { routerReducer } from 'react-router-redux'
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state,
-  router: routerReducer
+  state: (state = {}) => state
 })
 
 export default rootReducer
